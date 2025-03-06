fc197402daddc5080fdd139aea074b10782f7fbe
Vincent Morrill <vmorrilll@gmail.com>
2025-03-06 15:02:03 -0600

    feat: add ejs-loader for EJS template support and update publicPath handling in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
-const publicPath = isProduction ? '/Personal-Static/' : '/';
+const publicPath = process.env.CUSTOM_DOMAIN ? '/' : (isProduction ? '/Personal-Static/' : '/');
 
 
 module.exports = {
@@ -19,29 +19,41 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    publicPath: '/',
+    publicPath: publicPath,
     clean: true
   },
-
+  // 2.5) EJS loader for the pesky partials...
+  module: {
+    rules: [{
+      test: /\.ejs$/,
+      use: [{
+        loader: 'ejs-loader', 
+        options: {
+          esModule: false,
+          variable: 'data'
+        }
+      }]
+    }],
+  },
   devtool: 'source-map',
 
   // 3) Plugins
   plugins: [
     // Turn index.html into dist/index.html, injecting the script
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './views/index.html',
       filename: 'index.html',
       chunks: ['main']
     }),
     // Turn projects.html into dist/projects.html
     new HtmlWebpackPlugin({
-      template: './projects.html',
+      template: './views/projects.html',
       filename: 'projects.html',
       chunks: ['main']
     }),
     // Turn resources.html into dist/resources.html
     new HtmlWebpackPlugin({
-      template: './resources.html',
+      template: './views/resources.html',
       filename: 'resources.html',
       chunks: ['main']
     }),
@@ -49,7 +61,7 @@ module.exports = {
     // Copy static assets/folders into dist
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'partials/', to: 'partials/' },
+        // { from: 'partials/', to: 'partials/' },
         { from: 'rsc/', to: 'rsc/' },
         { from: 'style.css', to: 'style.css' },
         { from: 'css/', to: 'css/' },
